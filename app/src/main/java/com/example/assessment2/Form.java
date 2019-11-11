ee54af042110832b419e76afbe57ef973403e121
Kritik Ranjit <kritik.ranjit4@gmail.com>
2019-11-11 22:22:06 +0545

    Completion of User Registration Form

--- app/src/main/java/com/example/assessment2/Form.java
+++ app/src/main/java/com/example/assessment2/Form.java
@@ -10,5 +10,6 @@ public class Form extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+        getSupportActionBar().setTitle("User Registration Form");
     }
 }
